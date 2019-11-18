faef8d63274cecb19f2ec21dc9a8fc2db8c99eff
stanleyjobson <stanleyjobson728@gmail.com>
2019-11-18 16:27:37 +0800

    first commit

---
+++ src/com/demo/day01/Demo01.java
@@ -0,0 +1,14 @@
+package com.demo.day01;
+
+/**
+ * @Auther: ghost
+ * @Date: 2019/11/18 16:23
+ * @Description:
+ */
+public class Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println("seven day works");
+    }
+
+}
